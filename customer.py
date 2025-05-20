ba5becaeb02fe531ebc2ab5b438344b6f425d9f6
Neema <neema.edward@student.moringaschool.com>
2025-05-20 10:34:44 +0300

    ensures number is between 15 characters long

--- customer.py
+++ customer.py
@@ -2,7 +2,11 @@ class Customer:
     def __init__(self, name):
         self.name = name
         if not isinstance(name, str):
-            print("Name must be a string")
+            print("Name must be a string!")
+            return
+        
+        if len(name) < 1 or len(name) > 15:
+            print("Number should be 1 to 15 characters long!")
             return
         
         

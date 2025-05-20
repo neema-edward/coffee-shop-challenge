5766317dc39e90fd2889582b362adea48d4ed6fd
Neema <neema.edward@student.moringaschool.com>
2025-05-20 22:51:36 +0300

    updates order.py

--- order.py
+++ order.py
@@ -1,8 +1,12 @@
 class Order:
+
+    all_orders = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all_orders.append(self)
 
         if not isinstance (price, float):
             raise TypeError("Price is not float!")
@@ -11,17 +15,26 @@ class Order:
             raise TypeError("Price is off range")
         
     @property
-    def name(self):
+    def price(self):
         return self.price
     
-    @name.setter
-    def name(self, new_coffee):
-        if isinstance (new_coffee, str):
-            self.coffee = new_coffee
-        else:
-            raise ValueError("Not a string!!")
+    @property
+    def customer(self):
+        return self._customer
+    
+    @property
+    def coffee(self):
+        return self._coffee
+    
     
 order = Order("Neema", "Mocha", 7.8)
 print(order.price)
 order.price = "8.0"
 print(order.price)
+
+
+
+ 
+
+
+

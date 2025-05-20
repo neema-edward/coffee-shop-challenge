5e750663906a92e5a3916a83a09304a6893d3726
Neema <neema.edward@student.moringaschool.com>
2025-05-20 14:55:05 +0300

    adds getter to coffee.py

--- coffee.py
+++ coffee.py
@@ -1,11 +1,15 @@
 class Coffee:
     def __init__(self, name):
-        self.name = name
+
         if not isinstance (name, str):
             print("Must be a string!")
         
         if len(name) < 3:
             raise TypeError("Should be atleast 3 characters long!")
+        self._name = name
+    @property
+    def name(self):
+        return self._name
         
 coffee1 = Coffee("Mocha")
 print(coffee1.name)
